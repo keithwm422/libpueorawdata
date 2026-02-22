0aa3a20bd34c981fcdfb6b2587b64cf2b582c775
jason yao <yao.966@osu.edu>
2026-02-22 21:00:04 +0000

    privates don't need to be in the config.cmake

--- cmake/pueorawdataConfig.cmake
+++ cmake/pueorawdataConfig.cmake
@@ -2,5 +2,3 @@ include("${CMAKE_CURRENT_LIST_DIR}/pueorawdataTargets.cmake")
 
 include(CMakeFindDependencyMacro)
 find_dependency(ZLIB REQUIRED)
-find_package(PostgreSQL) #optional
-find_package(SQLite3)    #optional
